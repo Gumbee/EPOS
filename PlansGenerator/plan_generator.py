43ac52f4283b79a22cd0a986f8d0e63e306bee14
Massimo Bortone <bortonem@student.ethz.ch>
2018-11-27 18:35:13 +0100

    fixed plan generation

--- PlansGenerator/plan_generator.py
+++ PlansGenerator/plan_generator.py
@@ -1,37 +1,34 @@
 #!/usr/bin/env python
 import numpy as np
-from random import randint
+import itertools
 
-no_agents = 400
-no_applicants = 400
+no_agents = 100
+no_applicants = 3
 
 file = open("Plans/agentData.info","r")
+
+options_applicants = 3
+options_price = 3
+price_delta = 20
 
 
 for i in range(no_agents):
 	data_line = file.readline()
 	data_list = data_line.split(",")
-	for j in range(no_applicants):
+	applicant_ids = np.random.randint(0, high=no_applicants, size=options_applicants)
+	prices = np.array([float(data_list[2])-price_delta, float(data_list[2]), float(data_list[2])+price_delta])
+	plansfile = open("Plans/agent_" + str(i) + ".plan","w")
+	for j in applicant_ids:
 		applicant = np.zeros(no_applicants)
 		applicant[j] = 1.0
 		occupancy = np.zeros(no_agents)
-		occupancy[i] = randint(1,10)
-		price = np.zeros(no_agents)
-		price[i] = float(data_list[2]) - 20
-		plan1 = str(0.0) + ":" + np.array2string(applicant, separator=',')[1:-1] + "," + np.array2string(price, separator=',')[1:-1] + "," + np.array2string(occupancy, separator=',')[1:-1]+"\n"
-		price[i] = data_list[2]
-		plan2 = str(0.0) + ":" + np.array2string(applicant, separator=',')[1:-1] + "," + np.array2string(price, separator=',')[1:-1] + "," + np.array2string(occupancy, separator=',')[1:-1]+"\n"
-		price[i] = float(data_list[2]) + 20
-		plan3 = str(0.0) + ":" + np.array2string(applicant, separator=',')[1:-1] + "," + np.array2string(price, separator=',')[1:-1] + "," + np.array2string(occupancy, separator=',')[1:-1]+"\n"
-
-	#price = str(float(data_list[2])-20) + "," + str(float(data_list[2])-10) + "," + data_list[2] + "," + str(float(data_list[2])+10) + "," + str(float(data_list[2])+20)
-		if j == 0:
-			plansfile = open("Plans/agent_" + str(i) + ".plan","+w")
-		else:
-			plansfile = open("Plans/agent_" + str(i) + ".plan","a")
-	#plansfile.write(price)
-		plansfile.write(plan1+plan2+plan3)
-
-
-	#print (i)
-	#print (data_line)
+		occupancy[i] = np.random.randint(1, high=10)
+		for p in prices:
+			plan = str(0.0) + ":"
+			plan += np.array2string(applicant, separator=',')[1:-1]
+			price = np.zeros(no_agents)
+			price[i] = p
+			plan += "," + np.array2string(price, separator=',')[1:-1] + ","
+			plan += np.array2string(occupancy, separator=',')[1:-1]+"\n"
+			plansfile.write(plan)
+	plansfile.close()
