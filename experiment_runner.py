bea4d867ef4f71dbe4d3520d40d731bcd3d6cbe4
Mugeeb Hassan <mugeeb.hassan@gmail.com>
2018-12-01 16:34:13 +0100

    space-resistant code :D

--- experiment_runner.py
+++ experiment_runner.py
@@ -7,16 +7,19 @@ def set_settings(numAgents, numApplicants, lambdaValue, applicantOptions):
         new_lines = ""
 
         for line in lines:
-            if "numAgents=" in line:
+            spaceless_line = line.replace(" ", "")
+            if "#" in spaceless_line:
+                new_lines += line.replace("\n", "")
+            if "numAgents=" in spaceless_line:
                 new_lines += "numAgents=" + str(numAgents)
-            elif "numApplicants=" in line:
+            elif "numApplicants=" in spaceless_line:
                 new_lines += "numApplicants=" + str(numApplicants)
-            elif "weightsString=" in line:
+            elif "weightsString=" in spaceless_line:
                 new_lines += "weightsString=" + '"0.0,"' + str(lambdaValue)
-            elif "applicantOptions=" in line:
+            elif "applicantOptions=" in spaceless_line:
                 new_lines += "applicantOptions=" + str(applicantOptions)
             else:
-                new_lines += line.replace("\n", "")
+                new_lines += spaceless_line.replace("\n", "")
 
             new_lines += "\n"
 
